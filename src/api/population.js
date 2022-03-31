48ac579f07db74763001663cd6e015328e9d95a6
Gabriel Cerri <cerri367@gmail.com.ar>
2022-03-31 20:05:36 -0300

    Revert "fix api"
    This reverts commit 98058f8767d78a9f0694e0f127f78b6ecd898708.

--- src/api/population.js
+++ src/api/population.js
@@ -1,13 +1,12 @@
 export const getPopulation = async () => {
   const response = await window.fetch(
-    `https://restcountries.com/v3.1/region/americas`
+    `https://restcountries.eu/rest/v2/region/americas`
   );
 
   if (!response.ok) {
     throw new Error("Estamos teniendo problemas en el servidor, intenta luego");
   } else {
     const data = response.json();
-    console.log(data);
     return data;
   }
 };
